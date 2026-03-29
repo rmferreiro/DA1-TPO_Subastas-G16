1f96bdfa5d5aea8235c75901057b8ad479b0b267
Rodrigo Ferreiro <rodrigo.martin.ferreiro@gmail.com>
2026-03-29 01:13:05 -0300

    Initialize Spring

---
+++ src/main/java/ar/edu/uade/grupo16/subastas/SubastasApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.uade.grupo16.subastas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubastasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubastasApplication.class, args);
+	}
+
+}
